4f86f1af9f4d653ac29497c8a5ee9740120dc453
Alex Stuckey <alex@stuckey.org.uk>
2013-12-17 23:38:41 +0000

    Adding a footer as a basic template

---
+++ application/views/footer.php
@@ -0,0 +1,14 @@
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted">This project is in alpha</p>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bower_components/jquery/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
