b196dad0c30b76f966f6af3c908b339e7185d556
Alex Stuckey <alex@stuckey.org.uk>
2013-12-18 00:16:35 +0000

    Adding the static content for the homepage

---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+      <div class="container">
+        <div class="page-header">
+          <h1>Welcome to the Culture Bulletin</h1>
+        </div>
+        <p class="lead">You can use this app to manage the culture bulletin, not just for the next week, but for future weeks as well.</p>
+        
+        <h3>How to use this site</h3>
+        <ul>
+          <li> In the <a href="schedule">schedule</a> you can view and create <strong>Quarters</strong> as and when you need them.</li>
+          <li>Quarters contain under them <strong>weeks</strong>. Quarters are given start and end dates, from this the weeks (with their own dates) are extrapolated.</li>
+          <li><strong>Items</strong> are individual entries in a bulletin. These are not shown in the overview schedule, but are attached inside weeks.</li>
+        </ul>
+
+        <h3>Sending the bulletin</h3>
+        <p>A time is set by the administrator to send the bulletin out at each week. Just before it is sent out to the whole school, a final draft is sent to the Head of Culture. He can then make any alterations before the bulletin is sent out.</p>
+
+      </div>
+    </div>
