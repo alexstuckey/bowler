8f6e6421c6448143f869a41f18dc22fa75c3a4ec
Alex Stuckey <alex@stuckey.org.uk>
2013-12-19 00:44:11 +0000

    Form validation

--- application/views/quarter_creator.php
+++ application/views/quarter_creator.php
@@ -3,14 +3,27 @@
           <h1>Schedule <small>Create a quarter</small></h1>
         </div>
 
-        <form class="form-horizontal" role="form">
+        <?php
+        if (isset($failedBefore) && $failedBefore) {
+          print('<div class="panel panel-danger">
+          <div class="panel-heading">
+            <h3 class="panel-title">There was something wrong with the data you submitted</h3>
+          </div>
+          <div class="panel-body">');
+            print(validation_errors());
+            print('</div>
+        </div>');
+        }
+        ?>
+
+        <form class="form-horizontal" role="form" action="<?php echo base_url(); ?>schedule/sendCreate/quarter" method="post">
           <div class="form-group">
             <label for="inputType" class="col-sm-2 control-label">Type</label>
             <div class="col-sm-4">
               <select class="form-control" id="inputType" name="inputType">
-                <option>Oration Quarter</option>
-                <option>Long Quarter</option>
-                <option>Cricket Quarter</option>
+                <option value="Oration Quarter" <?php if(isset($failedBefore) && $failedBefore){ echo set_select('inputType', 'Oration Quarter', TRUE); } ?>>Oration Quarter</option>
+                <option value="Long Quarter" <?php if(isset($failedBefore) && $failedBefore){ echo set_select('inputType', 'Long Quarter'); } ?>>Long Quarter</option>
+                <option value="Cricket Quarter" <?php if(isset($failedBefore) && $failedBefore){ echo set_select('inputType', 'Cricket Quarter'); } ?>>Cricket Quarter</option>
               </select>
             </div>
           </div>
