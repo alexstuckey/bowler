08a7c6b16078a7dd974052915994a5e847c0d8bb
Alex Stuckey <alex@stuckey.org.uk>
2013-12-19 00:46:32 +0000

    Backend form validation and handling for multiple create types

--- application/controllers/schedule.php
+++ application/controllers/schedule.php
@@ -10,9 +10,81 @@ class Schedule extends CI_Controller {
   }
 
   public function create() {
+
+    // Extract the type of object to create from the URL
+    $type = $this->uri->segment(3, 'quarter');
+
     $this->load->view('header');
-    $this->load->view('quarter_creator');
+    
+    if ($type == 'quarter') {
+      $this->load->view('quarter_creator');
+    } elseif ($type == 'week') {
+      $this->load->view('week_creator');
+    }
+
     $this->load->view('footer');
+
+  }
+
+  public function sendCreate() {
+
+    // Extract the type of object to create from the URL
+    $type = $this->uri->segment(3, 'quarter');
+    
+    if ($type == 'quarter') {
+
+      $this->load->helper(array('form', 'url'));
+      $this->load->library('form_validation');
+
+      $formConfig = array(
+               array(
+                     'field'   => 'inputType', 
+                     'label'   => 'Type', 
+                     'rules'   => 'required'
+                  ),
+               array(
+                     'field'   => 'inputQuarterStart_submit', 
+                     'label'   => 'Date Start', 
+                     'rules'   => 'required'
+                  ),
+               array(
+                     'field'   => 'inputQuarterEnd_submit', 
+                     'label'   => 'Date End', 
+                     'rules'   => 'required'
+                  )
+            );
+
+      $this->form_validation->set_rules($formConfig);
+
+      if ($this->form_validation->run() == FALSE) {
+        // When failing, refil the form and show an error message
+        $passData['failedBefore'] = TRUE;
+
+        $this->load->view('header');
+        $this->load->view('quarter_creator', $passData);
+        $this->load->view('footer');
+      } else {
+        // Continue with inserting to database
+        $this->load->database();
+
+        $data = array(
+          'type' => $this->input->post('inputType'),
+          'start_date' => $this->input->post('inputQuarterStart_submit'),
+          'end_date' => $this->input->post('inputQuarterEnd_submit')
+          );
+
+        $this->db->insert('quarters', $data);
+
+        $this->session->set_flashdata('createdQuarter', TRUE);
+        redirect('/schedule/');
+      }
+
+    } elseif ($type == 'week') {
+
+      $this->load->view('week_creator');
+
+    }
+
   }
 
 }
