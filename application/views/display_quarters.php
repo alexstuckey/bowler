5edd1f9572b8ec3bdd0ea3c3be980847a82189a7
Alex Stuckey <alex@stuckey.org.uk>
2013-12-20 19:57:46 +0000

    New schedule list
    * Adding drop down button
    * Using Bootstrap to collapse the bodies
    * Put in (currently static) indicators for the number of items in each week.
    * Displays term dates, and half-term dates

--- application/views/display_quarters.php
+++ application/views/display_quarters.php
@@ -3,17 +3,29 @@
           <h1>Schedule <small>List</small></h1>
         </div>
 
-        {quarters}
-        <div class="col-sm-12">
+        <div class="panel-group" id="accordion">
+          {quarters}
           <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">{type}</h3> {start_date} — {end_date}
+              <div class="panel-heading quarterHeading">
+                <h3 class="panel-title">{type}</h3>
+                {start_date} — {end_date}
+                <br>
+                {half_start} — {half_end}
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse{id}">
+                  <i class="fa fa-caret-square-o-down pull-right"></i>
+                </a>
+              </div>
+            <div id="collapse{id}" class="panel-collapse collapse">
+              <div class="panel-body">
+                <button type="button" class="btn btn-danger">Delete</button>
+                {numberOfWeeks}: {numberOfWeeksOfHalfTerm}
+              </div>
             </div>
-              <ul class="list-group">
-                <li class="list-group-item"><span class="badge">1</span>Week 1</li>
-              </ul>
+            <ul class="list-group">
+              <li class="list-group-item"><span class="badge">1</span>Week 1</li>
+            </ul>
           </div>
+          {/quarters}
         </div>
-        {/quarters}
-
+        
       </div>
