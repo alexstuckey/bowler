f0e9dc96fa1cc0e35ef371486b1e170869753f66
Alex Stuckey <alex@stuckey.org.uk>
2014-01-02 00:54:17 +0000

    Trying another design for an item

--- application/views/display_item.php
+++ application/views/display_item.php
@@ -19,6 +19,17 @@
               </div>
             </li>
           {/items}
+            <li class="media">
+              <a class="pull-left">
+                1.
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">{summary}</h4>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? 
+                <br>
+                <small title="{user_email}">by {user_publicName}</small>
+              </div>
+            </li>
           </ul>
 
       </div>
